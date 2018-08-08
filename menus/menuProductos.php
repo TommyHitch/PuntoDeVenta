f8d5f96f6d186f91aeb91940bcd4278e3017d743
TommyHitch <TommyHitch@users.noreply.github.com>
2018-08-08 13:49:37 -0500

    Create menuProductos.php

---
+++ menus/menuProductos.php
@@ -0,0 +1,15 @@
+
+	<ul class="nav nav-tabs nav-justified">
+	<li class="nav-item">
+		<a class="nav-link" href="agregarProducto.php">AgregarNuevo</a>
+	</li>	
+	<li class="nav-item">
+	<a class="nav-link" href="enterProducto.php">Modificar</a>
+    </li>
+    <li class="nav-item">
+	<a class="nav-link" href="eliminarProducto.php">Eliminar</a>
+    </li>
+	<li class="nav-item">
+	<a class="nav-link" href="">menu2</a>
+     </li>
+    </ul>
