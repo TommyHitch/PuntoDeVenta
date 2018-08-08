c88256e48fc33c8291bf2d44e0db81ba9a1780c2
TommyHitch <TommyHitch@users.noreply.github.com>
2018-08-08 13:48:15 -0500

    Create menuPrincipal.php

---
+++ menus/menuPrincipal.php
@@ -0,0 +1,17 @@
+	<ul class="nav nav-tabs nav-justified">
+	<li class="nav-item">
+		<a class=" nav-link" href="index.php"><i class="fa fa-home"></i></a>
+	</li>	
+	<li class="nav-item">
+		<a class="nav-link" href="">Ventas</a>
+	</li>	
+	<li class="nav-item">
+	<a class="nav-link" href="">Clientes</a>
+    </li>
+    <li class="nav-item">
+	<a class="nav-link" href="agregarProducto.php">Productos</a>
+    </li>
+	<li class="nav-item">
+	<a class="nav-link" href="">Inventario</a>
+     </li>
+    </ul>
